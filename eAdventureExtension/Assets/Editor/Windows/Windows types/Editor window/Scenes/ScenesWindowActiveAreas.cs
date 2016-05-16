776405fc9e622e46ad5825abb4df7bdc4901668d
Piotr Marszał <piotr.marszal25@gmail.com>
2016-05-16 22:49:49 +0200

    * added active area and effect editor in screen active area view

--- eAdventureExtension/Assets/Editor/Windows/Windows types/Editor window/Scenes/ScenesWindowActiveAreas.cs
+++ eAdventureExtension/Assets/Editor/Windows/Windows types/Editor window/Scenes/ScenesWindowActiveAreas.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using  UnityEditor;
 using System.Collections;
 using System;
 
@@ -17,17 +18,22 @@ public class ScenesWindowActiveAreas : LayoutWindow, DialogReceiverInterface
 
     private static float windowWidth, windowHeight;
     private static Rect tableRect;
+    private static Rect actionRect;
     private static Rect previewRect;
     private static Rect infoPreviewRect;
     private Rect rightPanelRect;
+    private Rect actionRightPanelRect;
 
     private static Vector2 scrollPosition;
+    private static Vector2 scrollPositionAction;
 
     private static GUISkin selectedAreaSkin;
     private static GUISkin defaultSkin;
     private static GUISkin noBackgroundSkin;
 
     private int selectedArea;
+    private int selectedAction;
+    private AddItemActionMenu addMenu;
 
     public ScenesWindowActiveAreas(Rect aStartPos, GUIContent aContent, GUIStyle aStyle,
         params GUILayoutOption[] aOptions)
@@ -59,10 +65,13 @@ public class ScenesWindowActiveAreas : LayoutWindow, DialogReceiverInterface
 
         tableRect = new Rect(0f, 0.1f*windowHeight, 0.9f*windowWidth, windowHeight*0.33f);
         rightPanelRect = new Rect(0.9f*windowWidth, 0.1f*windowHeight, 0.08f*windowWidth, 0.33f*windowHeight);
-        infoPreviewRect = new Rect(0f, 0.45f*windowHeight, windowWidth, windowHeight*0.05f);
-        previewRect = new Rect(0f, 0.5f*windowHeight, windowWidth, windowHeight*0.45f);
+        infoPreviewRect = new Rect(0f, 0.65f*windowHeight, windowWidth, windowHeight*0.05f);
+        previewRect = new Rect(0f, 0.7f*windowHeight, windowWidth, windowHeight*0.25f);
+        actionRect = new Rect(0f, 0.45f*windowHeight, 0.9f*windowWidth, windowHeight*0.2f);
+        actionRightPanelRect = new Rect(0.9f*windowWidth, 0.45f*windowHeight, 0.1f*windowWidth, windowHeight*0.2f);
 
-        selectedArea = 0;
+        selectedArea = -1;
+        selectedAction = -1;
     }
 
     public override void Draw(int aID)
@@ -71,8 +80,7 @@ public class ScenesWindowActiveAreas : LayoutWindow, DialogReceiverInterface
         GUILayout.BeginHorizontal();
         GUILayout.Box("ID", GUILayout.Width(windowWidth*0.54f));
         GUILayout.Box("Conditions", GUILayout.Width(windowWidth*0.14f));
-        GUILayout.Box("Actions", GUILayout.Width(windowWidth*0.09f));
-        GUILayout.Box("Documentation", GUILayout.Width(windowWidth*0.09f));
+        GUILayout.Box("Documentation", GUILayout.Width(windowWidth*0.18f));
         GUILayout.EndHorizontal();
         scrollPosition = GUILayout.BeginScrollView(scrollPosition);
         for (int i = 0;
@@ -90,24 +98,22 @@ public class ScenesWindowActiveAreas : LayoutWindow, DialogReceiverInterface
                 GameRources.GetInstance().selectedSceneIndex].getActiveAreasList().getActiveAreasList()[i].getId(),
                 GUILayout.Width(windowWidth*0.54f)))
             {
-                selectedArea = i;
+                OnSelectionChanged(i);
             }
 
             if (GUILayout.Button(conditionTex, GUILayout.Width(windowWidth*0.14f)))
             {
-                selectedArea = i;
+                OnSelectionChanged(i);
+
                 ConditionEditorWindow window =
-                    (ConditionEditorWindow)ScriptableObject.CreateInstance(typeof(ConditionEditorWindow));
+                    (ConditionEditorWindow) ScriptableObject.CreateInstance(typeof (ConditionEditorWindow));
                 window.Init(Controller.getInstance().getSelectedChapterDataControl().getScenesList().getScenes()[
-                GameRources.GetInstance().selectedSceneIndex].getActiveAreasList().getActiveAreasList()[i].getConditions());
+                    GameRources.GetInstance().selectedSceneIndex].getActiveAreasList().getActiveAreasList()[i]
+                    .getConditions());
             }
-            if (GUILayout.Button("Edit actions", GUILayout.Width(windowWidth*0.09f)))
+            if (GUILayout.Button("Edit documentation", GUILayout.Width(windowWidth*0.18f)))
             {
-                selectedArea = i;
-            }
-            if (GUILayout.Button("Edit documentation", GUILayout.Width(windowWidth*0.09f)))
-            {
-                selectedArea = i;
+                OnSelectionChanged(i);
             }
 
             GUILayout.EndHorizontal();
@@ -126,7 +132,7 @@ public class ScenesWindowActiveAreas : LayoutWindow, DialogReceiverInterface
         if (GUILayout.Button(addTexture, GUILayout.MaxWidth(0.08f*windowWidth)))
         {
             ActiveAreaNewName window =
-                  (ActiveAreaNewName)ScriptableObject.CreateInstance(typeof(ActiveAreaNewName));
+                (ActiveAreaNewName) ScriptableObject.CreateInstance(typeof (ActiveAreaNewName));
             window.Init(this, "IdObject");
         }
         if (GUILayout.Button(duplicateImg, GUILayout.MaxWidth(0.08f*windowWidth)))
@@ -161,9 +167,157 @@ public class ScenesWindowActiveAreas : LayoutWindow, DialogReceiverInterface
                 .deleteElement(Controller.getInstance().getSelectedChapterDataControl().getScenesList().getScenes()[
                     GameRources.GetInstance().selectedSceneIndex].getActiveAreasList().getActiveAreas()[selectedArea],
                     false);
+            if (selectedArea >= 0)
+                selectedArea--;
+            if (selectedAction >= 0)
+                selectedAction--;
         }
         GUI.skin = defaultSkin;
         GUILayout.EndArea();
+
+        /**
+        * ACTION EDITOR
+        */
+        GUILayout.BeginArea(actionRect);
+        GUILayout.BeginHorizontal();
+        GUILayout.Box("Action", GUILayout.Width(windowWidth*0.39f));
+        GUILayout.Box("Needs to go", GUILayout.Width(windowWidth*0.39f));
+        GUILayout.Box("Effects/conditions", GUILayout.Width(windowWidth*0.1f));
+        GUILayout.EndHorizontal();
+        scrollPositionAction = GUILayout.BeginScrollView(scrollPositionAction, GUILayout.ExpandWidth(false));
+        if (selectedArea >= 0)
+        {
+            // Action table
+            for (int i = 0;
+                i < Controller.getInstance().getSelectedChapterDataControl().getScenesList().getScenes()[
+                    GameRources.GetInstance().selectedSceneIndex].getActiveAreasList().getActiveAreas()[selectedArea]
+                    .getActionsList().getActions().Count;
+                i++)
+            {
+                if (i == selectedAction)
+                    GUI.skin = selectedAreaSkin;
+                else
+                    GUI.skin = noBackgroundSkin;
+
+                GUILayout.BeginHorizontal();
+                if (i == selectedAction)
+                {
+
+                    GUILayout.Label(
+                        Controller.getInstance().getSelectedChapterDataControl().getScenesList().getScenes()[
+                            GameRources.GetInstance().selectedSceneIndex].getActiveAreasList().getActiveAreas()[
+                                selectedArea
+                            ].getActionsList().getActions()[i].getTypeName(),
+                        GUILayout.Width(windowWidth*0.39f));
+
+                    //TODO: distinguish between first/third person
+
+                    //Controller.getInstance().getSelectedChapterDataControl().getItemsList().getItems()[
+                    //GameRources.GetInstance().selectedItemIndex].getActionsList().getActions()[i].getNeedsGoTo().ToString()
+                    if (GUILayout.Button("Not relevant", GUILayout.Width(windowWidth*0.39f)))
+                    {
+                        selectedAction = i;
+                    }
+                    GUILayout.BeginVertical();
+                    if (GUILayout.Button("Conditons", GUILayout.Width(windowWidth*0.1f)))
+                    {
+                        ConditionEditorWindow window =
+                            (ConditionEditorWindow) ScriptableObject.CreateInstance(typeof (ConditionEditorWindow));
+                        window.Init(
+                            Controller.getInstance().getSelectedChapterDataControl().getScenesList().getScenes()[
+                                GameRources.GetInstance().selectedSceneIndex].getActiveAreasList().getActiveAreas()[
+                                    selectedArea
+                                ].getActionsList().getActions()[i].getConditions());
+                    }
+                    if (GUILayout.Button("Effects", GUILayout.Width(windowWidth*0.1f)))
+                    {
+                        EffectEditorWindow window =
+                            (EffectEditorWindow) ScriptableObject.CreateInstance(typeof (EffectEditorWindow));
+                        window.Init(
+                            Controller.getInstance().getSelectedChapterDataControl().getScenesList().getScenes()[
+                                GameRources.GetInstance().selectedSceneIndex].getActiveAreasList().getActiveAreas()[
+                                    selectedArea
+                                ].getActionsList().getActions()[i].getEffects());
+                    }
+                    if (GUILayout.Button("Not-effects", GUILayout.Width(windowWidth*0.1f)))
+                    {
+                        EffectEditorWindow window =
+                            (EffectEditorWindow) ScriptableObject.CreateInstance(typeof (EffectEditorWindow));
+                        window.Init(
+                            Controller.getInstance().getSelectedChapterDataControl().getScenesList().getScenes()[
+                                GameRources.GetInstance().selectedSceneIndex].getActiveAreasList().getActiveAreas()[
+                                    selectedArea
+                                ].getActionsList().getActions()[i].getNotEffectsController());
+                    }
+                    GUILayout.EndVertical();
+                }
+                else
+                {
+                    if (
+                        GUILayout.Button(
+                            Controller.getInstance().getSelectedChapterDataControl().getScenesList().getScenes()[
+                                GameRources.GetInstance().selectedSceneIndex].getActiveAreasList().getActiveAreas()[
+                                    selectedArea].getActionsList().getActions()[i].getTypeName(),
+                            GUILayout.Width(windowWidth*0.39f)))
+                    {
+                        selectedAction = i;
+                    }
+
+                    //TODO: distinguish between first/third person
+
+                    //Controller.getInstance().getSelectedChapterDataControl().getItemsList().getItems()[
+                    //GameRources.GetInstance().selectedItemIndex].getActionsList().getActions()[i].getNeedsGoTo().ToString()
+                    if (GUILayout.Button("Not relevant", GUILayout.Width(windowWidth*0.39f)))
+                    {
+                        selectedAction = i;
+                    }
+                    if (GUILayout.Button("Edit", GUILayout.Width(windowWidth*0.1f)))
+                    {
+                        selectedAction = i;
+                    }
+                }
+                GUILayout.EndHorizontal();
+                GUI.skin = defaultSkin;
+            }
+        }
+
+        GUILayout.EndScrollView();
+        GUILayout.EndArea();
+
+        /*
+        * Right action panel
+        */
+        GUILayout.BeginArea(actionRightPanelRect);
+        GUI.skin = noBackgroundSkin;
+        if (GUILayout.Button(addTexture, GUILayout.MaxWidth(0.08f*windowWidth)))
+        {
+            addMenu.menu.ShowAsContext();
+        }
+        if (GUILayout.Button(duplicateImg, GUILayout.MaxWidth(0.08f*windowWidth)))
+        {
+            Controller.getInstance().getSelectedChapterDataControl().getScenesList().getScenes()[
+                GameRources.GetInstance().selectedSceneIndex].getActiveAreasList().getActiveAreas()[selectedArea]
+                .getActionsList()
+                .duplicateElement(Controller.getInstance().getSelectedChapterDataControl().getScenesList().getScenes()[
+                    GameRources.GetInstance().selectedSceneIndex].getActiveAreasList().getActiveAreas()[selectedArea]
+                    .getActionsList().getActions()[selectedAction]);
+        }
+        if (GUILayout.Button(clearImg, GUILayout.MaxWidth(0.08f*windowWidth)))
+        {
+            Controller.getInstance().getSelectedChapterDataControl().getScenesList().getScenes()[
+                GameRources.GetInstance().selectedSceneIndex].getActiveAreasList().getActiveAreas()[selectedArea]
+                .getActionsList()
+                .deleteElement(Controller.getInstance().getSelectedChapterDataControl().getScenesList().getScenes()[
+                    GameRources.GetInstance().selectedSceneIndex].getActiveAreasList().getActiveAreas()[selectedArea]
+                    .getActionsList().getActions()[selectedAction], false);
+            if (selectedAction >= 0)
+                selectedAction--;
+        }
+        GUI.skin = defaultSkin;
+        GUILayout.EndArea();
+
+
+
 
 
         if (backgroundPath != "")
@@ -190,6 +344,13 @@ public class ScenesWindowActiveAreas : LayoutWindow, DialogReceiverInterface
         }
     }
 
+    private void OnSelectionChanged(int i)
+    {
+        selectedArea = i;
+        selectedAction = -1;
+        addMenu = new AddItemActionMenu(i);
+    }
+
     public void OnDialogOk(string message, object workingObject = null, object workingObjectSecond = null)
     {
         Debug.Log("Apply");
@@ -205,4 +366,126 @@ public class ScenesWindowActiveAreas : LayoutWindow, DialogReceiverInterface
     {
         Debug.Log("Cancel");
     }
+
+    #region Add item action options
+
+    class AddItemActionMenu : WindowMenuContainer
+    {
+        private AddUseAction useAction;
+        private AddExamineAction examineAction;
+        private AddGrabAction grabAction;
+        private AddCustomAction customAction;
+        private static int selectedActiveArea;
+
+        public static int GetSelectedActiveArea()
+        {
+            return selectedActiveArea;
+        }
+
+        public AddItemActionMenu(int i)
+        {
+            selectedActiveArea = i;
+            SetMenuItems();
+        }
+
+        protected override void Callback(object obj)
+        {
+            if ((obj as AddUseAction) != null)
+                useAction.OnCliked();
+            else if ((obj as AddExamineAction) != null)
+                examineAction.OnCliked();
+            else if ((obj as AddGrabAction) != null)
+                grabAction.OnCliked();
+            else if ((obj as AddCustomAction) != null)
+                customAction.OnCliked();
+        }
+
+        protected override void SetMenuItems()
+        {
+            menu = new GenericMenu();
+
+            useAction = new AddUseAction("Add \"Use\" action");
+            examineAction = new AddExamineAction("Add \"Examine\" action");
+            grabAction = new AddGrabAction("Add \"Grab\" action");
+            customAction = new AddCustomAction("Add \"Custom\" action");
+
+
+            menu.AddItem(new GUIContent(useAction.Label), false, Callback, useAction);
+            menu.AddItem(new GUIContent(examineAction.Label), false, Callback, examineAction);
+            menu.AddItem(new GUIContent(grabAction.Label), false, Callback, grabAction);
+            menu.AddItem(new GUIContent(customAction.Label), false, Callback, customAction);
+        }
+    }
+
+    class AddUseAction : IMenuItem
+    {
+        public AddUseAction(string name_)
+        {
+            this.Label = name_;
+        }
+
+        public string Label { get; set; }
+
+        public void OnCliked()
+        {
+            Controller.getInstance().getSelectedChapterDataControl().getScenesList().getScenes()[
+                GameRources.GetInstance().selectedSceneIndex].getActiveAreasList().getActiveAreas()[
+                    AddItemActionMenu.GetSelectedActiveArea()].getActionsList().addElement(Controller.ACTION_USE, "");
+        }
+    }
+
+    class AddExamineAction : IMenuItem
+    {
+        public AddExamineAction(string name_)
+        {
+            this.Label = name_;
+        }
+
+        public string Label { get; set; }
+
+        public void OnCliked()
+        {
+            Controller.getInstance().getSelectedChapterDataControl().getScenesList().getScenes()[
+                GameRources.GetInstance().selectedSceneIndex].getActiveAreasList().getActiveAreas()[
+                    AddItemActionMenu.GetSelectedActiveArea()].getActionsList()
+                .addElement(Controller.ACTION_EXAMINE, "");
+        }
+    }
+
+    class AddGrabAction : IMenuItem
+    {
+        public AddGrabAction(string name_)
+        {
+            this.Label = name_;
+        }
+
+        public string Label { get; set; }
+
+        public void OnCliked()
+        {
+            Controller.getInstance().getSelectedChapterDataControl().getScenesList().getScenes()[
+                GameRources.GetInstance().selectedSceneIndex].getActiveAreasList().getActiveAreas()[
+                    AddItemActionMenu.GetSelectedActiveArea()].getActionsList().addElement(Controller.ACTION_GRAB, "");
+        }
+    }
+
+    class AddCustomAction : IMenuItem
+    {
+        public AddCustomAction(string name_)
+        {
+            this.Label = name_;
+        }
+
+        public string Label { get; set; }
+
+        public void OnCliked()
+        {
+
+            Controller.getInstance().getSelectedChapterDataControl().getScenesList().getScenes()[
+                GameRources.GetInstance().selectedSceneIndex].getActiveAreasList().getActiveAreas()[
+                    AddItemActionMenu.GetSelectedActiveArea()].getActionsList().addElement(Controller.ACTION_CUSTOM, "");
+        }
+    }
+
+    #endregion
 }
